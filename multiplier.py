3026189475e67444dd33beb706b9e4edbb64306e
christine chemali <christine.chemali@laplateforme.io>
2025-02-10 18:51:12 +0100

    Add steel type multipliers to type_multiplier dictionary
    This commit adds a 	ype_multiplier dictionary that defines the effectiveness of the steel type against various Pokémon types.

--- multiplier.py
+++ multiplier.py
@@ -1,1 +1,23 @@
 # Pokemon multiplier type dictionnary
+
+type_multiplier = {
+    ('steel', 'steel'): 1,
+    ('steel', 'fighting'): 2,
+    ('steel', 'dragon'): 1,
+    ('steel', 'water'): 1,
+    ('steel', 'electric'): 1,
+    ('steel', 'fairy'): 1,
+    ('steel', 'fire'): 0.5,
+    ('steel', 'ice'): 1,
+    ('steel', 'bug'): 1,
+    ('steel', 'normal'): 1,
+    ('steel', 'grass'): 1,
+    ('steel', 'poison'): 1,
+    ('steel', 'psychic'): 1,
+    ('steel', 'rock'): 1,
+    ('steel', 'ground'): 1,
+    ('steel', 'ghost'): 1,
+    ('steel', 'dark'): 1,
+    ('steel', 'flying'): 1,
+
+}
