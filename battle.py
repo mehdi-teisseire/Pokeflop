7db681b2df69b469888692c039908fddfd6e191f
christine chemali <christine.chemali@laplateforme.io>
2025-02-11 11:27:06 +0100

    Add choose_attack method to Battle class
    This commit introduces the choose_attack method within the Battle class, which allows players to choose an attack for their Pokémon during a battle. This method utilizes a graphical interface to present attack options and handles user input.

--- battle.py
+++ battle.py
@@ -1,4 +1,10 @@
 import json
+import random
+import pygame
+import sys
+from pokemon import Pokemon
+from multiplier import type_multiplier
+from utils import Button
 
 
 
@@ -30,4 +36,35 @@ class Battle:
             return self.current_pokemon_1.name, self.current_pokemon_2.name
         elif self.current_pokemon_2 <= 0:
             return self.current_pokemon_1.name, self.current_pokemon_2.name
-        return None, None   
+        return None, None  
+
+    # To allow the player to choose an attack
+    def choose_attack(screen, self, pokemon):
+        button1 = Button(pokemon.moov1, 100, 300, 200, 50) # should be changed
+        button2 = Button(pokemon.moov2, 400, 300, 200, 50) # should be changed
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = event.pos
+                    if button1.is_clicked(mouse_pos):
+                        return pokemon.moov1, pokemon.moove_type_1
+                    elif button2.is_clicked(mouse_pos):
+                        return pokemon.moov2, pokemon.moove_type_2
+            screen.fill(Button.WHITE)
+            option_text = self.font.render(f"{pokemon.name}, choose your attack:", True, self.BLUE) # Should be changed
+            instruction_line1 = self.font.render("Press 1 :" + pokemon.moov1, True, self.BLUE) # Should be changed
+            instruction_line2 = self.font.render("Press 2 :" + pokemon.moov2, True, self.BLUE) # Should be changed
+
+            screen.blit(option_text, (100, 200))
+            screen.blit(instruction_line1, (100, 250))
+            screen.blit(instruction_line2 (100, 280))
+
+            button1.draw(screen)
+            button2.draw(screen)
+            pygame.display.flip()
+            
+            
+
