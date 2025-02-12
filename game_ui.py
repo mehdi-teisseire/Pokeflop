c49e13314a4600fbd69b1c5a790a6eb17cd38147
christine chemali <christine.chemali@laplateforme.io>
2025-02-12 07:42:13 +0100

    feat: Add clear_screen method to GameUI class
    Introduces clear_screen method for clearing the screen with a specified color. Defaults to white (255, 255, 255) for a clean background. Enhances rendering management for the user interface.

--- game_ui.py
+++ game_ui.py
@@ -7,6 +7,11 @@ class GameUI:
         self.screen = screen
         self.font = pygame.font.Font(None, 36)
 
-def draw_text(self, text, position, color=(0, 0, 0)):
-    rendered_text= self.font.render(text,True, color, position)
-    self.screen.blit(rendered_text, position)
+    # To draw text
+    def draw_text(self, text, position, color=(0, 0, 0)):
+        rendered_text= self.font.render(text,True, color, position)
+        self.screen.blit(rendered_text, position)
+
+    # To clear the screen
+    def clear_screen(self, color=(255, 255, 255)):
+        self.screen.fill(color)
