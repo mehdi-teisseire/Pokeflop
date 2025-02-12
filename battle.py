87c826dd2fd6b81b87c2ecc595f6ff95479f07cf
christine chemali <christine.chemali@laplateforme.io>
2025-02-12 10:35:29 +0100

    Fix : implement render_message on choose_attack to correctly display the messages

--- battle.py
+++ battle.py
@@ -39,7 +39,7 @@ class Battle:
         return None, None  
 
     # To allow the player to choose an attack
-    def choose_attack(self, screen, pokemon):
+    def choose_attack(self, screen, font, pokemon):
         button1 = Button(pokemon.moov1, 100, 300, 200, 50) # should be changed
         button2 = Button(pokemon.moov2, 400, 300, 200, 50) # should be changed
         while True:
@@ -51,13 +51,9 @@ class Battle:
                     elif button2.is_clicked(mouse_pos):
                         return pokemon.moov2, pokemon.moove_type_2
             screen.fill(Button.WHITE)
-            option_text = self.font.render(f"{pokemon.name}, choose your attack:", True, self.BLUE) # Should be changed
-            instruction_line1 = self.font.render("Press 1 :" + pokemon.moov1, True, self.BLUE) # Should be changed
-            instruction_line2 = self.font.render("Press 2 :" + pokemon.moov2, True, self.BLUE) # Should be changed
-
-            screen.blit(option_text, (100, 200))
-            screen.blit(instruction_line1, (100, 250))
-            screen.blit(instruction_line2 (100, 280))
+            render_message(screen, font, f"{pokemon.name}, choose your attack!")
+            render_message(screen, font, "Press 1 :" + pokemon.moov1)
+            render_message(screen, font,"Press 2 :" + pokemon.moov2)
 
             button1.draw(screen)
             button2.draw(screen)
