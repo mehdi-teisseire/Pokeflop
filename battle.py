588da4b53935acd87c3b8da0fc0052a190fdbfb7
christine chemali <christine.chemali@laplateforme.io>
2025-02-12 22:03:20 +0100

    Refactor battle.py no more pygame elements in it

--- battle.py
+++ battle.py
@@ -1,10 +1,7 @@
 import json
 import random
-import pygame
 from pokemon import *
 from multiplier import type_multiplier
-from utils import *
-from game_ui import GameUI
 
 
 
@@ -39,89 +36,48 @@ class Battle:
         return None, None  
             
     # To calculate damage based on the attacker's chosen move
-    def calculate_damage(self, attacker, defender, move_type, screen, font):
+    def calculate_damage(self, attacker, defender, move_type):
         if random.random() < 0.1:
-            message = f"{attacker.name}'s attack missed!"
-            render_message(screen, message, font)
             return 0  
         damage = attacker.attack
         multiplier = type_multiplier.get((move_type, defender.pkm_type), 1)
         effective_damage = damage * multiplier
         defender.take_damage(effective_damage)
-
-        message = f"{attacker.name} dealt {effective_damage} damage!"
-        render_message(screen, message, font)
         return effective_damage 
 
     # To display the defeat
-    def display_defeat(self, pokemon, screen, font):
-        message = f"{pokemon.name} has been defeated."
-        render_message(screen, message, font)
+    def display_defeat(self, pokemon):
         self.pokedex = [p for p in self.pokedex if p['name'] != pokemon.name]  # Retirer le Pokémon du Pokédex
         self.save_pokedex()
 
     # To display the victory
-    def display_victory(self, pokemon, screen, font):
-        message = f"{pokemon.name} is victorious!"
-        render_message(screen, message, font)
+    def display_victory(self, pokemon):
         pokemon.level_up() 
     
     # To change the active pokemon in the battle
-    def switch_pokemon(self, new_pokemon, screen, font):
+    def switch_pokemon(self, new_pokemon):
         if new_pokemon in self.player_1_pokemons:
             self.current_pokemon_1 = new_pokemon
-            message = f"{self.current_pokemon_1.name} is now active for Player 1."
-            render_message(screen, message, font)
         elif new_pokemon in self.player_2_pokemons:
             self.current_pokemon_2 = new_pokemon
-            message = f"{self.current_pokemon_2.name} is now active for Player 2."
-            render_message(screen, message, font)
-        else:
-            message = f"The chosen Pokémon is not part of the player's team."
-            render_message(screen, message, font)  
 
-    # A button to surrender 
-    def surrender(self, screen, pokemon, font, player):
-        surrender_button = Button("Surrender", 250, 300, 200, 50)
-        screen.fill(Button.WHITE)
-        surrender_button.draw(screen)
-        pygame.display.flip()
-
-        for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = event.pos
-                if surrender_button.is_clicked(mouse_pos):
-                    message = f"{pokemon.name} has surrendered !"  
-                    render_message(screen, message, font)
-                    winner = self.current_pokemon_2.name if player == self.current_pokemon_1 else self.current_pokemon_1.name 
-                    message = f"{winner} wins by default!"
-                    return         
-
-    # To start the battle
-    def start_battle(self, screen, font):
-        ui = GameUI(screen)
-        ui.clear_screen()
-        ui.draw_text("A wild Pokémon appeared!")
-        pygame.display.flip()
-        pygame.time.delay(2000)
+    # To Start the battle
+    def start_battle(self):
         turn = 0
         while self.current_pokemon_1.life > 0 and self.current_pokemon_2.life > 0:
             if turn % 2 == 0:
-                move, move_type = self.choose_attack(screen, self.current_pokemon_1)
-                damage = self.calculate_damage(self.current_pokemon_1, self.current_pokemon_2, move_type, screen, self.font)
-                message = f"{self.current_pokemon_1.name} uses {move} on {self.current_pokemon_2.name} and inflicts {damage} damage."
-                render_message(screen, message, font)
+                attacker = self.current_pokemon_1
+                defender = self.current_pokemon_2
             else:
-                move, move_type = self.choose_attack(screen, self.current_pokemon_2)
-                damage = self.calculate_damage(self.current_pokemon_2, self.current_pokemon_1, move_type, screen, self.font)
-                message = f" {self.current_pokemon_2.name} uses {move} on {self.current_pokemon_1.name} and inflicts {damage} damage."
-                render_message(screen, message, font)
+                attacker = self.current_pokemon_2
+                defender = self.current_pokemon_1
+
+            move, move_type = self.choose_attack(attacker)
+            damage = self.calculate_damage(attacker, defender, move_type)
+            defender.life -= damage  
 
             winner_name, loser_name = self.check_health_points()
             if winner_name:
-                message = f"{winner_name} won the battle against {loser_name}!"
-                render_message(screen, message, font)
-                self.display_victory(self.current_pokemon_1 if winner_name == self.current_pokemon_1 else self.current_pokemon_2, screen, self.font)
-                return
+                return winner_name, move, damage, loser_name
 
-            turn += 1                            
+            turn += 1
