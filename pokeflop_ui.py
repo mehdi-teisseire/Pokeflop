6ccbc13c11498ee517fd087a19f4cd254fd0ab8e
christine chemali <christine.chemali@laplateforme.io>
2025-02-12 23:07:11 +0100

    Scale images in POKEFLOP class to fit 800x400 screen while maintaining aspect ratios

--- pokeflop_ui.py
+++ pokeflop_ui.py
@@ -7,3 +7,9 @@ class POKEFLOP:
         self.background_image = pygame.image.load('media/picture/bg.png')
         self.logo_image = pygame.image.load('media/picture/PokeFlop-2-12-2025.png')
         self.start_text_image = pygame.image.load('media/picture/Press-space-to-start-2-12-2025.png')
+
+        self.background_image = pygame.image.load('media/picture/bg.png')
+        self.logo_image = pygame.image.load('media/picture/PokeFlop-2-12-2025.png')
+        self.start_text_image = pygame.image.load('media/picture/press_space_to_start-2-12-2025.png')
+
+        
