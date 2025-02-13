825306e30899f1f80908480bfd19948be12559f9
jeremy-gavend <jeremy.gavend@laplateforme.io>
2025-02-13 14:46:00 +0100

    fix: removed some useless lines to choose attack

--- ui/ingame.py
+++ ui/ingame.py
@@ -4,40 +4,29 @@ from ui.ButtonClass import Button
 from model.TrainerClass import Trainer
 from model.EnemyTrainerClass import EnemyTrainer
 
-def game():
-    
-    trainer1 = Trainer("Hello")
-    enemy1 = EnemyTrainer("Rival")
-
-    # while running:
-    #     pass
+def display_ingame(game):
+    #displaybackground()
+    #display_pokemon()
+    display_attack_choice(game)
 
 
 # To allow the player to choose an attack
-def display_attack_choice(screen, pokemon):
-    button1 = Button(pokemon.moov1, 100, 300, 200, 50) # should be changed
-    button2 = Button(pokemon.moov2, 400, 300, 200, 50) # should be changed
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = event.pos
-                if button1.is_clicked(mouse_pos):
-                    return pokemon.moov1, pokemon.moove_type_1
-                elif button2.is_clicked(mouse_pos):
-                    return pokemon.moov2, pokemon.moove_type_2
-        screen.fill("white")
-        option_text = self.font.render(f"{pokemon.name}, choose your attack:", True, self.BLUE) # Should be changed
-        instruction_line1 = self.font.render("Press 1 :" + pokemon.moov1, True, self.BLUE) # Should be changed
-        instruction_line2 = self.font.render("Press 2 :" + pokemon.moov2, True, self.BLUE) # Should be changed        
-        
-        screen.blit(option_text, (100, 200))
-        screen.blit(instruction_line1, (100, 250))
-        screen.blit(instruction_line2 (100, 280))
-        
-        button1.draw(screen)
-        button2.draw(screen)
-        
-        pygame.display.flip()
+def display_attack_choice(game):
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = event.pos
+            if game.button_moov1.is_clicked(mouse_pos):
+                return game.turn_pkmn.moov1
+            elif game.button_moov2.is_clicked(mouse_pos):
+                return game.turn_pkmn.moov2
+            
+    game.screen.fill("white")
+
+    option_text = game.font.render(f"{game.pokemon.name}, choose your attack:", True, "blue") # Should be changed
+
+    game.screen.blit(option_text, (100, 200))
+    
+    game.button_moov1.draw(game.screen)
+    game.button_moov2.draw(game.screen)
+    
+    pygame.display.flip()
